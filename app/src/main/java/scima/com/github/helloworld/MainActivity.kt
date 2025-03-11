c15317fbb8ba11a8d4af498cf74498d2eb1039ac
sCima <44555125+sCima@users.noreply.github.com>
2025-03-10 22:27:21 -0300

    Add files via upload

--- app/src/main/java/scima/com/github/helloworld/MainActivity.kt
+++ app/src/main/java/scima/com/github/helloworld/MainActivity.kt
@@ -1,21 +1,33 @@
 package scima.com.github.helloworld
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import scima.com.github.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main) 
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHelloMessage = findViewById<TextView>(R.id.textViewHello)
+
+        etName.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val name = textView.text.toString().trim()
+
+                if (name.isNotEmpty()) {
+                    tvHelloMessage.text = "Hello World, $name!"
+                } else {
+                    tvHelloMessage.text = "Hello World!"
+                }
+                true
+            } else {
+                false
+            }
+        }
+
+        setContentView(R.layout.activity_main)
     }
 }
